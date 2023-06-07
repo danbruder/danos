c34601ec5158b70c2cd152cc5ef0c337b5836a74
Dan Bruder <danbruder@hey.com>
2023-06-07 15:00:06 -0400

    wip

--- assets/src/Pages/Home_.elm
+++ assets/src/Pages/Home_.elm
@@ -1,11 +1,39 @@
 module Pages.Home_ exposing (page)
 
-import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes as Attr exposing (css)
+import Tailwind.Breakpoints as Breakpoints
+import Tailwind.Theme as Tw
+import Tailwind.Utilities as Tw
+import Ui
 import View exposing (View)
 
 
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body =
+        [ Ui.layout
+            { sidebar =
+                [ div
+                    [ css
+                        [ Tw.bg_color Tw.gray_50
+                        , Tw.p_2
+                        ]
+                    ]
+                    [ text "Hello, DanOs"
+                    ]
+                ]
+            , content =
+                [ div
+                    [ css
+                        [ Tw.w_full
+                        , Tw.p_2
+                        ]
+                    ]
+                    [ text "Hello, DanOs"
+                    ]
+                ]
+            }
+        ]
     }
